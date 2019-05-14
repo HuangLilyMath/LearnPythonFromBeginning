0f77f72009bdd93ee7f35243b5ffb8ad644de97f
Huang Jinli <huangjinli7253@163.com>
2019-05-14 22:55:21 +0800

    数字

--- draft.py
+++ draft.py
@@ -1,5 +1,11 @@
-# 第一个程序 hello world
+import math
+math.pi
 
-print("hello,world")
+#这里的math相当于一个模块。math模块包括一些高级的数学工具。
 
-# ✌
+#random模块可以作为随机数字的生成器
+
+import random
+random.random()
+random.choice([1,2,3,4,5,6])
+
